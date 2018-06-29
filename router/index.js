37b53b22969c00197d32c57d1f875ad6b9a1ce13
lvwenbin@youyuwo.com <lvwenbin@youyuwo.com>
2018-06-29 18:07:46 +0800

    文章上传接口

--- router/index.js
+++ router/index.js
@@ -1,5 +1,9 @@
 const tag = require("./tag.js");
+const blogs = require("./blogs.js");
 
 module.exports = function(app) {
-  app.get("/tag", tag);
+  app.route("/tag").get(tag.get).post(tag.post);
+  app.route("/tag/:id").patch(tag.patch).delete(tag.delete).get(tag.getByCode);
+  app.route("/blogs").get(blogs.get).post(blogs.post);
+  app.route("/blogs/:id").patch(blogs.patch).delete(blogs.delete);
 };
