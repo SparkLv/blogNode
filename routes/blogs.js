1bd0735b2184dbeab6b14cafeb0498b0d71e7c4c
lvwenbin@youyuwo.com <lvwenbin@youyuwo.com>
2018-07-27 18:36:52 +0800

    七牛cdn上传

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,13 +1,42 @@
 const sql = require("../database/sql/blogs.js");
+const tagSql = require("../database/sql/tag.js");
 const con = require("../database/index.js");
 const util = require("./util");
 
+function getTagsObj(blogs,tags){
+  const tagIdMap = tags.map(item=>item.id);
+  const result = blogs.map(item=>{
+    const arr = item.tags.split(',');
+    item.tagsO = arr.map(item=>{
+      const index = tagIdMap.indexOf(parseInt(item,10));
+      return tags[index];
+    })
+    return item
+  })
+  return result
+}
+
 module.exports = {
   get: function(req, res) {
-    con(sql.query,[],function(result){res.send(result)},function(err){util.res400(err,res)});
+    con(sql.query,[],function(blogs){
+      con(tagSql.query,[],function(tags){
+        res.send(getTagsObj(blogs,tags));
+      },function(err){util.res400(err,res)});
+    },function(err){util.res400(err,res)});
   },
   getById:function(req,res){
     con(sql.queryById,[req.params.id],function(result){res.send(result[0])},function(err){util.res400(err,res)});
+  },
+  getByPage:function(req,res){
+    con(sql.queryByPage,[(req.body.pageNum-1)*req.body.pageSize,req.body.pageSize],function(result){
+      con(sql.getTotal,[],function(result2){
+        const obj = {
+          total:JSON.parse(JSON.stringify(result2))[0]["count(*)"],
+          data:result
+        }
+        res.send(obj)
+      },function(err){util.res400(err,res)})
+    },function(err){util.res400(err,res)});
   },
   post: function(req, res) {
     const param = [req.body.title,req.body.content,req.body.tags,req.body.imgUrl,req.body.updateTime,req.body.remark];
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const tag = require("./tag.js");
 const blogs = require("./blogs.js");
+const upload = require('./upload.js');
 
 module.exports = function(app) {
   app.route("/tag").get(tag.get).post(tag.post);
   app.route("/tag/:id").patch(tag.patch).delete(tag.delete).get(tag.getByCode);
   app.route("/blogs").get(blogs.get).post(blogs.post);
   app.route("/blogs/:id").patch(blogs.patch).delete(blogs.delete).get(blogs.getById);
+  app.route('/blogs/getByPage').post(blogs.getByPage);
+  app.route('/blog/upload').post(upload.upload);
 };
